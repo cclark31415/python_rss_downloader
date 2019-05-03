3cf0cdf80dcfb04c076349b5e21b170341005881
Chris Clark <cclark@ALTYVJG5H.local>
2019-05-03 12:34:23 -0400

    Storing filenames

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Downloads all files from an RSS feed.  Also includes a progress bar
 
 For testing purposes, this only grabs the first three items to save on the feed's bandwidth (the costs add up FAST).  If you have a feed where you want to grab all items, replace the for-range loop with the commented-out line `for post in feed.entries:`
 
+This will store the processed filenames in a text file to prevent duplicate downloads.  If the file does not already exist, it will create it.
--- rss_feed_downloader.py
+++ rss_feed_downloader.py
@@ -4,6 +4,11 @@ import requests
 from clint.textui import progress
 
 url = 'http://feed.theskepticsguide.org/feed'
+processed_file='processed_files.txt'
+
+processed_list = {}
+
+processed_data=open(processed_file, "a+")
 
 feed = feedparser.parse(url)
 
@@ -12,9 +17,19 @@ feed = feedparser.parse(url)
 # For testing, let's only grab the first 3 to save their bandwidth.  
 # Otherwise, use the for statement above
 for i in range(3):
+
+    #Make sure we have the latest list to avoid duplicates
+    processed_data=open(processed_file, "r")
+    if(processed_data is not None):
+        processed_list = set(map(str.strip, processed_data))
+    processed_data.close()
+
     post = feed.entries[i]
     url = post.links[0].href
     file_name = url.split('/')[-1]
+    if(file_name in processed_list):
+        continue
+
     f = open(file_name, 'wb')
 
     r = requests.get(url, stream=True)
@@ -24,3 +39,8 @@ for i in range(3):
         for ch in progress.bar(r.iter_content(chunk_size = 2391975), expected_size=(total_length/1024) + 1):
             if ch:
                 file.write(ch)
+    
+    processed_data=open(processed_file, "a+")
+    processed_data.write(file_name)
+    processed_data.write('\n')
+    processed_data.close()
